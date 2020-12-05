1a032584020e089cc987da38949b8e97dfc8a5d4
RoshanJose7 <roshanstark007@gmail.com>
2020-12-05 19:22:01 +0530

    Add: Forgot Password Route

--- server/src/Routers/users.ts
+++ server/src/Routers/users.ts
@@ -6,12 +6,12 @@ import { userModel } from '../DB/models/user';
 export const UserRouter = express.Router();
 dotenv.config();
 
-UserRouter.get('/users', async (req: Request, res: Response<{}>) => {
+UserRouter.get('/users', async (req: Request, res: Response) => {
 	const users = await userModel.find({});
 	res.send(users);
 });
 
-UserRouter.get('/dropDB', async (req: Request, res: Response<{}>) => {
+UserRouter.get('/dropDB', async (req: Request, res: Response) => {
 	await db.dropDatabase();
 	res.send({ msg: 'Db dropped' });
 });
@@ -32,11 +32,11 @@ UserRouter.post('/user/login', async (req: Request, res: Response) => {
 	});
 });
 
-UserRouter.get('/user/logout', (req: Request, res: Response<string>) => {
+UserRouter.get('/user/logout', (req: Request, res: Response) => {
 	res.send('LogOut');
 });
 
-UserRouter.post('/user/signup', async (req: Request, res: Response<{}>) => {
+UserRouter.post('/user/signup', async (req: Request, res: Response) => {
 	const { name, usn, password } = req.body;
 	const dbUser = await userModel.findOne({ name });
 
@@ -54,7 +54,7 @@ UserRouter.post('/user/signup', async (req: Request, res: Response<{}>) => {
 	return res.status(401).send({ error: 'Error creating user' });
 });
 
-UserRouter.get('/user/forgotpassword', async (req: Request, res: Response) => {
+UserRouter.patch('/user/forgotpassword', async (req: Request, res: Response) => {
 	const name = req.body.name;
 	const usn = req.body.usn;
 	const password = req.body.password;
