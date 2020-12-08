7fdf727aab69e17a78760415b38df4e982387498
4TfCl5P7X7ce <roshanstark007@gmail.com>
2020-12-08 19:15:37 +0530

    Fix: gap in NavBar

--- client/src/pages/Home.pages.tsx
+++ client/src/pages/Home.pages.tsx
@@ -1,12 +1,12 @@
 // import { motion } from 'framer-motion';
-import '../styles/Home.styles.scss';
+import "../styles/Home.styles.scss";
 
 const HomePage: React.FC = () => {
-	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Hello Roshan</h1>
+    </div>
+  );
 };
 
 export default HomePage;
