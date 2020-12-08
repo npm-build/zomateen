ebc56553a2acfdff5b2ec6c3dfd4b30f3fa3c013
4TfCl5P7X7ce <roshanstark007@gmail.com>
2020-12-08 20:08:26 +0530

    Add: Subtle Animations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import Header from './components/Header';
-import Navbar from './components/NavBar';
-import Home from './pages/Home.pages';
-import { Route, Switch } from 'react-router-dom';
-import { AnimateSharedLayout } from 'framer-motion';
-import './styles/Home.styles.scss';
+import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/NavBar";
+import AboutPage from "./pages/About.pages";
+import Home from "./pages/Home.pages";
+import { Route, Switch, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import "./styles/Home.styles.scss";
 
 function App() {
-	return (
-		<AnimateSharedLayout>
-			<div id='home-grid'>
-				<Navbar />
-				<Header />
-				<div id='content'>
-					<Switch>
-						<Route path='/' exact component={Home} />
-					</Switch>
-				</div>
-			</div>
-		</AnimateSharedLayout>
-	);
+  const location = useLocation();
+
+  return (
+    <div id="home-grid">
+      <Navbar />
+      <Header />
+      <div id="content">
+        <AnimatePresence exitBeforeEnter>
+          <Switch location={location} key={location.pathname}>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={AboutPage} />
+          </Switch>
+        </AnimatePresence>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- client/src/pages/About.pages.tsx
+++ client/src/pages/About.pages.tsx
@@ -2,29 +2,20 @@ import { motion } from "framer-motion";
 import cool from "../assets/img/cool.png";
 import "../styles/Home.styles.scss";
 
-const HomePage: React.FC = () => {
-  const product = "cool";
+const AboutPage: React.FC = () => {
+  //   const product = "cool";
   return (
     <motion.div
       exit={{ opacity: 0, x: "-100vw", scale: 0.8 }}
       initial={{ opacity: 0, x: "-100vw", scale: 0.8 }}
       animate={{ opacity: 1, x: 0, scale: 1 }}
       transition={{ type: "tween", ease: "anticipate", duration: 1 }}
-      className="cool-id"
+      id="cool-id"
     >
-      <h1>Hello World</h1>
-      <a href="/about">Go to About</a>
-      {/* <a href="/about">
-        <motion.img
-          layoutId={product}
-          animate={{ scale: 1 }}
-          whileHover={{ scale: 1.1 }}
-          src={cool}
-          alt="COOL"
-        />
-      </a> */}
+      <h1>About Page</h1>
+      <a href="/">Go Home</a>
     </motion.div>
   );
 };
 
-export default HomePage;
+export default AboutPage;
